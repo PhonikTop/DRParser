057478568a212c49df35aec220d75e4e47a534e2
PhonikTop <askar.a.shakirov@gmail.com>
2023-11-17 21:45:31 +0300

    New version of parser

--- main.py
+++ main.py
@@ -1,36 +1,51 @@
 import asyncio
 import aiohttp
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
 
 async def fetch_html(url, user_agent=None):
     headers = {"User-Agent": user_agent} if user_agent else {}
     async with aiohttp.ClientSession() as session:
-        async with session.get(url, headers=headers) as response:
-            return await response.text()
+        try:
+            async with session.get(url, headers=headers) as response:
+                response.raise_for_status()  # добавляем для обработки ошибок
+                return await response.text()
+        except aiohttp.ClientError as e:
+            print(f"Error during HTTP request: {e}")
+            return None
 
 
 async def get_product_info(url):
-    user_agent = (
-        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0"
-    )
+    user_agent = UserAgent().random
     html = await fetch_html(url, user_agent)
-    soup = BeautifulSoup(html, "html.parser").find(
-        "div", class_="facet__products-list js-facet-product-list"
-    )
-    container = soup.find_all("div", class_="prd-main-wrapper")
-    for obj in container:
-        name = obj.find("h3", class_="seo-heading").text.strip()
-        autor = obj.find("a", class_="who font-weight-bold").text.strip()
-        price = obj.find("div", class_="prd-price").text.strip()
-        print(f"{name}\n{autor}\n{price}\n\n")
+    if html:
+        soup = BeautifulSoup(html, "html.parser").find(
+            "div", class_="facet__products-list js-facet-product-list"
+        )
+        if soup:
+            container = soup.find_all("div", class_="prd-main-wrapper")
+            for obj in container:
+                name = obj.find("h3", class_="seo-heading").text.strip()
+                author = obj.find(
+                    "a", class_="who font-weight-bold"
+                ).text.strip()  # изменяем название переменной на author
+                price = obj.find("div", class_="prd-price").text.strip()
+                product_info = f"{name}\n{author}\n{price}\n\n"
+                print(product_info)  # выводим информацию о продукте
+        else:
+            print(
+                "Unable to find product list on the page"
+            )  # обрабатываем случай, когда контейнер продуктов не найден
+    else:
+        print(
+            "Failed to fetch HTML from the URL"
+        )  # обрабатываем случай, когда HTML не был получен
 
 
 async def main():
     url = "https://www.dr.com.tr/search?Page=1&Q=Python&SortOrder=1&SortType=0"
-
-    product_info = await get_product_info(url)
-    print(product_info)
+    await get_product_info(url)
 
 
 if __name__ == "__main__":
